9b2b29e727307f0e018958eec1116138d4f76450
Nathan Woltman <nwoltman@outlook.com>
2015-10-26 23:11:48 -0700

    Add tool for generating documentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var fs = require('fs');
+
 module.exports = function(grunt) {
   grunt.initConfig({
     eslint: {
@@ -29,13 +31,36 @@ module.exports = function(grunt) {
         require: 'should',
       },
     },
+
+    jsdoc2md: {
+      docs: {
+        options: {
+          partial: [
+            'jsdoc2md/partials/body.hbs',
+            'jsdoc2md/partials/examples.hbs',
+            'jsdoc2md/partials/link.hbs',
+            'jsdoc2md/partials/linked-type-list.hbs',
+            'jsdoc2md/partials/params-table.hbs',
+            'jsdoc2md/partials/param-table-name.hbs',
+            'jsdoc2md/partials/separator.hbs',
+          ],
+          separators: true,
+          'sort-by': ['name'],
+          template: fs.readFileSync('jsdoc2md/README.hbs', {encoding: 'utf8'}),
+        },
+        src: 'callback-manager.js',
+        dest: 'README.md',
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-eslint');
   grunt.loadNpmTasks('grunt-mocha-cov');
+  grunt.loadNpmTasks('grunt-jsdoc-to-markdown');
 
   grunt.registerTask('lint', ['eslint']);
   grunt.registerTask('test', ['mochacov:test'].concat(process.env.CI ? ['mochacov:ciCoverage'] : []));
   grunt.registerTask('coverage', ['mochacov:coverage']);
+  grunt.registerTask('doc', ['jsdoc2md']);
   grunt.registerTask('default', ['lint', 'test']);
 };
