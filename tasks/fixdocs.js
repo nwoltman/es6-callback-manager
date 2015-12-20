f030f57392b70a5ac8d6d01911108de194a1bece
Nathan Woltman <nwoltman@outlook.com>
2015-12-20 16:24:47 -0500

    tasks: Revert extra code from 0942687 to fix documentation
    The package that caused the problem has been updated so the
    fixing code is no longer needed.

--- tasks/fixdocs.js
+++ tasks/fixdocs.js
@@ -4,10 +4,8 @@ var fs = require('fs');
 var os = require('os');
 
 module.exports = function(grunt) {
-  grunt.registerTask('fixdocs', 'Fixes any problems with the generated documenation', function() {
-    var docs = fs.readFileSync('README.md', 'utf8')
-      .replace(/\r\n|\r|\n/g, os.EOL)
-      .replace(/([\s\S]{3000,})\1\1/g, '$1');
+  grunt.registerTask('fixdocs', 'Fixes any problems with the generated documentation', function() {
+    var docs = fs.readFileSync('README.md', 'utf8').replace(/\r\n|\r|\n/g, os.EOL);
     fs.writeFileSync('README.md', docs);
     grunt.log.ok('Fixed docs');
   });
