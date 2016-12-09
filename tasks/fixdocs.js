4becc0eebcfc12974b26d25766f19ce94a7d0b14
Nathan Woltman <nwoltman@outlook.com>
2016-12-08 23:36:47 -0500

    tasks: Improve fixdocs task
    Now the task will left-align param table columns and make code examples look better

--- tasks/fixdocs.js
+++ tasks/fixdocs.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var fs = require('fs');
-var os = require('os');
+const fs = require('fs');
+const os = require('os');
 
 module.exports = function(grunt) {
-  grunt.registerTask('fixdocs', 'Fixes any problems with the generated documentation', function() {
-    var docs = fs.readFileSync('README.md', 'utf8').replace(/\r\n|\r|\n/g, os.EOL);
+  grunt.registerTask('fixdocs', 'Fixes any problems with the generated documentation', () => {
+    const docs = fs.readFileSync('README.md', 'utf8')
+      .replace(/\r\n|\r|\n/g, os.EOL)
+      .replace(/\| --- /g, '|:--- ')
+      .replace(new RegExp('  ' + os.EOL + '(?=\\*\\*Example\\*\\*)', 'g'), os.EOL + os.EOL);
     fs.writeFileSync('README.md', docs);
     grunt.log.ok('Fixed docs');
   });
